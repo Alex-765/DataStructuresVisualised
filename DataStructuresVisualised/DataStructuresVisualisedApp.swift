8c47e52885be99afe8e15d871be16242f26df69a
alexanderbater <bater.a@etoncollege.org.uk>
2023-03-15 10:42:34 +0000

    Initial Commit

---
+++ DataStructuresVisualised/DataStructuresVisualisedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresVisualisedApp.swift
+//  DataStructuresVisualised
+//
+//  Created by Alexander Bater on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresVisualisedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
